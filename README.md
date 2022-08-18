55f6ad46eb798a24d9a704638bbf563213664a72
comradexlight <comradexlight@gmail.com>
2022-08-18 21:08:13 +0300

    added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/qfhrc5hjlt3w9t8q?svg=true)](https://ci.appveyor.com/project/comradexlight/ajs-hw6-for-in)
---
+++ src/js/orderByProps.js
@@ -0,0 +1,11 @@
+function orderByProps(object, Array) {
+	let sortedPropsArray = [];
+	for (const prop in object) {
+
+	}
+	return sortedPropsArray;
+};
+
+const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+
+console.log(orderByProps(obj, []));
