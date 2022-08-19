56bd2b3e941150e70f610745490f2dc0da4ffc0f
comradexlight <comradexlight@gmail.com>
2022-08-19 16:53:58 +0300

    fixed order function and added test

---
+++ src/js/__tests__/orderByPropsTest.js
@@ -0,0 +1,16 @@
+import orderByProps from '../orderByProps';
+
+test.each([
+  [{
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  }, ['level', 'defence'], [
+    { key: 'level', value: 2 },
+    { key: 'defence', value: 40 },
+    { key: 'attack', value: 80 },
+    { key: 'health', value: 10 },
+    { key: 'name', value: 'мечник' },
+  ]],
+])('testeing orderByProps function with %o character and %l fisrtPropsArray and %l expected', (character, fisrtPropsArray, expected) => {
+  const result = orderByProps(character, fisrtPropsArray);
+  expect(result).toEqual(expected);
+});
--- src/js/orderByProps.js
+++ src/js/orderByProps.js
@@ -1,32 +1,16 @@
-function orderByProps(object, Array) {
-	const entries = Object.entries(object)
-	entries.sort(function (a, b) {
-		for (const element in Array) {
-			if (Array[element] === a || Array[element] === b) {
-				return 1
-			}
-			if (a > b) {
-				return 1
-			}
-			if (a < b) {
-				return -1
-			}
-			return 0
-		}
-	});
-	console.log(entries);
-	let sortedPropsArray = [];
-	for (const element in entries) {
-		sortedPropsArray.push({ 'key': entries[element][0], 'value': entries[element][1] })
-	}
-	
-	// for (const prop in object) {
-	// 	sortedPropsArray.push({ 'key': prop, 'value': object[prop] })
-	// 	console.log()
-	// }
-	return sortedPropsArray;
-};
-
-const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
-console.log(orderByProps(obj, ['health', 'attack']));
-
+export default function orderByProps(object, fisrtPropsArray) {
+  const order = [];
+  const sortedPropsArray = [];
+  fisrtPropsArray.forEach((element) => {
+    order.push(element);
+  });
+  const keys = Object.keys(object).sort();
+  keys.forEach((element) => {
+    order.push(element);
+  });
+  const uniqueOrder = Array.from(new Set(order));
+  uniqueOrder.forEach((element) => {
+    sortedPropsArray.push({ key: element, value: object[element] });
+  });
+  return sortedPropsArray;
+}
