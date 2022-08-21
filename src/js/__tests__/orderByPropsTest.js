7638a25e09c979fcd39b75b3e3f1c6e9760e7a5c
comradexlight <comradexlight@gmail.com>
2022-08-21 17:17:53 +0300

    added tests

--- src/js/__tests__/orderByPropsTest.js
+++ src/js/__tests__/orderByPropsTest.js
@@ -10,6 +10,24 @@ test.each([
     { key: 'health', value: 10 },
     { key: 'name', value: 'мечник' },
   ]],
+  [{
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  }, ['name'], [
+    { key: 'name', value: 'мечник' },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+  ]],
+  [{
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  }, [], [
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ]],
 ])('testeing orderByProps function with %o character and %l fisrtPropsArray and %l expected', (character, fisrtPropsArray, expected) => {
   const result = orderByProps(character, fisrtPropsArray);
   expect(result).toEqual(expected);
